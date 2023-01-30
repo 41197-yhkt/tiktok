9734dc18c13314ec084dbdbca5911147020f612b
haoeliu@foxmail.com <haoeliu@foxmail.com>
2023-01-30 21:54:43 +0800

    test: update trace.go and add trace_test.go

--- pkg/trace/trace.go
+++ pkg/trace/trace.go
@@ -2,27 +2,28 @@ package trace
 
 import (
 	"fmt"
-
-	opentracing "github.com/opentracing/opentracing-go"
-	jaeger "github.com/uber/jaeger-client-go"
-	config "github.com/uber/jaeger-client-go/config"
+	"github.com/uber/jaeger-client-go"
+	"github.com/uber/jaeger-client-go/config"
+	"io"
 )
 
-func InitJaeger(service string) {
-	cfg := &config.Configuration{
-		ServiceName: service,
+// initJaeger 将jaeger tracer设置为全局tracer
+func initJaeger(service string) io.Closer {
+	cfg := config.Configuration{
+		// 将采样频率设置为1，每一个span都记录，方便查看测试结果
 		Sampler: &config.SamplerConfig{
-			Type:  "const",
+			Type:  jaeger.SamplerTypeConst,
 			Param: 1,
 		},
 		Reporter: &config.ReporterConfig{
 			LogSpans: true,
+			// 将span发往jaeger-collector的服务地址，需要拉一个jaeger起来（详情看docker-compose）
+			// CollectorEndpoint: "http://localhost:14268/api/traces",
 		},
 	}
-	tracer, _, err := cfg.NewTracer(config.Logger(jaeger.StdLogger))
+	closer, err := cfg.InitGlobalTracer(service, config.Logger(jaeger.StdLogger))
 	if err != nil {
 		panic(fmt.Sprintf("ERROR: cannot init Jaeger: %v\n", err))
 	}
-	opentracing.SetGlobalTracer(tracer)
-	return
+	return closer
 }
