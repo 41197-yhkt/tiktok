47752d6d1ef245550add9cde2e15abfeafd8b168
haoeliu@foxmail.com <haoeliu@foxmail.com>
2023-02-03 10:23:48 +0800

    refactor: change the db table struct

--- script/sql/initUser.sql
+++ script/sql/initUser.sql
@@ -1,34 +1,48 @@
 USE tiktok_db;
 
--- user_name作为登录的唯一账号
-CREATE TABLE user(
-    id INT NOT NULL AUTO_INCREMENT,
-    user_name VARCHAR(20) NOT NULL,
-    user_pwd_hash  VARCHAR(32) NOT NULL,
-    follow_count INT,
-    follower_count INT,
-    PRIMARY KEY (id),
-    UNIQUE(user_name)
-);
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+                         `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+                         `created_at` datetime(3) DEFAULT NULL,
+                         `updated_at` datetime(3) DEFAULT NULL,
+                         `deleted_at` datetime(3) DEFAULT NULL,
+                         `user_name` longtext,
+                         `user_pwd_hash` longtext,
+                         `follow_count` bigint DEFAULT NULL,
+                         `follower_count` bigint DEFAULT NULL,
+                         PRIMARY KEY (`id`),
+                         KEY `idx_users_deleted_at` (`deleted_at`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+SET FOREIGN_KEY_CHECKS = 1;
+
 
 -- 考虑到用户改名，所以follow用id定位用户
--- 增加单向唯一约束，即'用户A只能follow用户B一次'
 -- 传入时应用层不需排序
 -- 未设置外键
-CREATE TABLE user_follow_relation(
-    id INT NOT NULL AUTO_INCREMENT,
-    follow_from INT NOT NULL,
-    follow_to INT NOT NULL,
-    PRIMARY KEY (id),
-    UNIQUE KEY follow_relation (follow_from, follow_to)
-);
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
--- friend是双向的，但是为了减少索引，应用层将user_a与user_b按大小排序之后再传入
-CREATE TABLE user_friend_relation(
-    id INT NOT NULL AUTO_INCREMENT,
-    user_a INT NOT NULL,
-    user_b INT NOT NULL,
-    PRIMARY KEY (id),
-    UNIQUE KEY friend_relation (user_a, user_b)
-);
+-- ----------------------------
+-- Table structure for user_relations
+-- ----------------------------
+DROP TABLE IF EXISTS `user_relations`;
+CREATE TABLE `user_relations` (
+                                  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+                                  `created_at` datetime(3) DEFAULT NULL,
+                                  `updated_at` datetime(3) DEFAULT NULL,
+                                  `deleted_at` datetime(3) DEFAULT NULL,
+                                  `follow_from` bigint unsigned DEFAULT NULL,
+                                  `follow_to` bigint unsigned DEFAULT NULL,
+                                  PRIMARY KEY (`id`),
+                                  KEY `idx_user_relations_deleted_at` (`deleted_at`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+SET FOREIGN_KEY_CHECKS = 1;
 
