451e20646638d38472ca17d57a38948facdaaaa2
root <root@LAPTOP-A39JN6MP>
2023-02-11 10:17:04 +0800

    fix: fix user bug(except login)

--- gateway/internal/handler/user.go
+++ gateway/internal/handler/user.go
@@ -7,8 +7,7 @@ import (
 
 	// "github.com/41197-yhkt/tiktok/gateway/kitex_gen/relation"
 	// "github.com/41197-yhkt/tiktok/gateway/kitex_gen/relation/relationservice"
-	"github.com/41197-yhkt/tiktok/gateway/kitex_gen/relation"
-	"github.com/41197-yhkt/tiktok/gateway/kitex_gen/relation/relationservice"
+	"github.com/41197-yhkt/tiktok/gateway/biz/model/douyin"
 	"github.com/41197-yhkt/tiktok/gateway/kitex_gen/user"
 	"github.com/41197-yhkt/tiktok/gateway/kitex_gen/user/userservice"
 	"github.com/41197-yhkt/tiktok/pkg/errno"
@@ -17,6 +16,7 @@ import (
 	etcd "github.com/kitex-contrib/registry-etcd"
 
 	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/cloudwego/hertz/pkg/common/hlog"
 	"github.com/cloudwego/hertz/pkg/protocol/consts"
 )
 
@@ -46,6 +46,10 @@ func DouyinUserRegisterMethod(ctx context.Context, c *app.RequestContext) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
 	resp, _ := client.UserRegister(ctx, &req)
 	cancel()
+	if err != nil {
+		SendResponse(c, *errno.ServerError)
+		return
+	}
 	if resp.BaseResp.StatusCode != 0 {
 		SendResponseWithErr(c, resp.BaseResp.StatusCode, *resp.BaseResp.StatusMsg)
 		return
@@ -56,39 +60,41 @@ func DouyinUserRegisterMethod(ctx context.Context, c *app.RequestContext) {
 
 }
 
-
-
-//TODO: 这个一点没动
+// TODO: 这个一点没动
 // DouyinUserLoginMethod .
 // @router /relation/user/login [POST]
 func DouyinUserLoginMethod(ctx context.Context, c *app.RequestContext) (interface{}, error) {
-	return &user.UserLoginResponse{
-		BaseResp: nil,
-		UserId:   2,
-	}, nil
-	// var err error
-	// var req user.UserLoginRequest
-	// err = c.BindAndValidate(&req)
-	// if err != nil {
-	// 	return nil, err
-	// }
-	// r, err := etcd.NewEtcdResolver([]string{"127.0.0.1:2379"})
-	// if err != nil {
-	// 	return nil, err
+	// return &user.UserLoginResponse{
+	// 	BaseResp: nil,
+	// 	UserId:   2,
+	// }, nil
+	var err error
+	var req user.UserLoginRequest
+	err = c.BindAndValidate(&req)
+	if err != nil {
+		return nil, err
+	}
+	r, err := etcd.NewEtcdResolver([]string{"127.0.0.1:2379"})
+	if err != nil {
+		return nil, err
+	}
+
+	client, err := userservice.NewClient("user", client.WithResolver(r))
+	if err != nil {
+		return nil, err
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	resp, err := client.UserLogin(ctx, &req)
+	cancel()
+	if err != nil {
+		return nil, err
+	}
+	// if resp.BaseResp.StatusCode != 0 {
+	// 	SendResponseWithErr(c, resp.BaseResp.StatusCode, *resp.BaseResp.StatusMsg)
+	// 	return nil, nil
 	// }
 
-	// client, err := userservice.NewClient("user", client.WithResolver(r))
-	// if err != nil {
-	// 	return nil, err
-	// }
-	// ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
-	// resp, err := client.UserLogin(ctx, &req)
-	// cancel()
-	// if err != nil {
-	// 	return nil, err
-	// }
-
-	// return resp, nil
+	return resp, nil
 }
 
 // DouyinUserMethod .
@@ -116,7 +122,7 @@ func DouyinUserMethod(ctx context.Context, c *app.RequestContext) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
 	resp, err := client.UserInfo(ctx, &req)
 	cancel()
-	if err != nil{
+	if err != nil {
 		SendResponse(c, *errno.ServerError)
 		return
 	}
@@ -124,33 +130,45 @@ func DouyinUserMethod(ctx context.Context, c *app.RequestContext) {
 		SendResponseWithErr(c, resp.BaseResp.StatusCode, *resp.BaseResp.StatusMsg)
 		return
 	}
+	//RPC和HTTP返回的一致，没有更改
 	c.JSON(consts.StatusOK, resp)
 }
 
 // DouyinRelationActionMethod .
 // @router /relation/relation/action [POST]
+// relation移动到了user中
 func DouyinRelationActionMethod(ctx context.Context, c *app.RequestContext) {
 	var err error
-	var req relation.DouyinRelationActionRequest
-	err = c.BindAndValidate(&req)
+	var reqHTTP douyin.DouyinRelationActionRequest
+	err = c.BindAndValidate(&reqHTTP)
 	if err != nil {
 		c.String(consts.StatusBadRequest, err.Error())
 		return
 	}
+
+	user_id := getUserIdFromJWT(ctx, c)
+	hlog.Info("to_user_id=", reqHTTP.ToUserID, " user_id=", user_id)
+
+	reqRPC := user.RelationActionRequest{
+		UserId:     user_id,
+		ToUserId:   reqHTTP.ToUserID,
+		ActionType: reqHTTP.ActionType,
+	}
+
 	r, err := etcd.NewEtcdResolver([]string{"127.0.0.1:2379"})
 	if err != nil {
 		SendResponse(c, *errno.ServerError)
 		return
 	}
-	client, err := relationservice.NewClient("user", client.WithResolver(r))
+	client, err := userservice.NewClient("user", client.WithResolver(r))
 	if err != nil {
 		SendResponse(c, *errno.ServerError)
 		return
 	}
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
-	resp, err := client.DouyinRelationActionMethod(ctx, &req)
+	resp, err := client.RealtionAction(ctx, &reqRPC)
 	cancel()
-	if err != nil{
+	if err != nil {
 		SendResponse(c, *errno.ServerError)
 		return
 	}
@@ -184,7 +202,7 @@ func DouyinRelationFollowListMethod(ctx context.Context, c *app.RequestContext) 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
 	resp, err := client.GetFollowList(ctx, &req)
 	cancel()
-	if err != nil{
+	if err != nil {
 		SendResponse(c, *errno.ServerError)
 		return
 	}
@@ -218,7 +236,7 @@ func DouyinRelationFollowerListMethod(ctx context.Context, c *app.RequestContext
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
 	resp, err := client.GetFollowerList(ctx, &req)
 	cancel()
-	if err != nil{
+	if err != nil {
 		SendResponse(c, *errno.ServerError)
 		return
 	}
@@ -253,7 +271,7 @@ func DouyinRelationFriendListMethod(ctx context.Context, c *app.RequestContext) 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
 	resp, err := client.GetFriendList(ctx, &req)
 	cancel()
-	if err != nil{
+	if err != nil {
 		SendResponse(c, *errno.ServerError)
 		return
 	}
